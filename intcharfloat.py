438b6f78f5f87115e3d509a7fe4bb3489fc6ac3e
DIVYA <sreedivya808@gmail.com>
2023-09-16 19:12:04 +0530

    intcharfloat.py

---
+++ intcharfloat.py
@@ -0,0 +1,7 @@
+a=300
+b=500
+divya="This is my first Message"
+
+print("\n given value is in interger Format",a)
+print("\n ginen value is in char Format:",b)
+print("\n given valuue is in float format;",divya)

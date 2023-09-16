b7e793c4be512f7124b5613f60fd5af7b3b46ece
DIVYA <sreedivya808@gmail.com>
2023-09-16 18:16:38 +0530

    sum.py

---
+++ comment.py
@@ -0,0 +1,5 @@
+a=1234 # first valuve declaration
+b=1245 #second value declaration
+c=1245 # third value declaration
+d=a+b*c
+print("the sum of three values are",d)
--- test.py
+++
@@ -1,5 +0,0 @@
-a=1 
-b=2
-c=3
-c=a+b+c
-print("the sum of 3 values",c)

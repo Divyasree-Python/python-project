9b87726d676cfae3b666d9e4fd07e029acc6a6d5
DIVYA <sreedivya808@gmail.com>
2023-09-26 18:57:00 +0530

    subject marks.py

--- all arth function.py
+++ all arth function.py
@@ -1,1 +1,19 @@
-sub+
+#enter marks obtained in 5 subjects
+
+english =30
+maths =70
+physics =50
+chemistry =80
+telugu=60
+hindi=89
+total = english+maths+physics+chemistry+telugu+hindi
+print("\n English Marks are",english)
+print("\n maths Marks are",maths)
+print("\n physics Marks are",physics)
+print("\n chemistry Marks are",chemistry)
+print("\n telugu Marks are",telugu)
+print("\n hindi Marks are",hindi)
+
+
+
+print("\n The average of 6 subject marks are ",)
